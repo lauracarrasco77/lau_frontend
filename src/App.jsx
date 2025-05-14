fce78d8379a5aa0ea3d9a9a3068513b0bfb3e7dd
Laura <lauracarrasco77@gmail.com>
2025-05-13 22:02:32 -0600

    nuevo cmpoente agregado

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Encabezado from "./components/encabezado/Encabezado";
 import Producto from "./views/productos";
 import Ventas from "./views/ventas";
 import Compras from "./views/compras";
+import Catalogo from "./views/CatalogoProductos";
 
 
 const App = () => {
@@ -26,6 +27,7 @@ const App = () => {
           <Route path="/categorias" element={<Categorias />} />
           <Route path="/ventas" element={<Ventas />} />
           <Route path="/compras" element={<Compras />} />
+           <Route path="/CatalogoProductos" element={<Catalogo />} />
         </Routes>
       </main>
     </Router>
